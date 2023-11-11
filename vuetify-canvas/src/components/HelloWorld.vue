3c57317ba2387340e21ddbdace5f168abee2690c
avh <andreas.vanhelden@conceto.de>
2023-11-11 01:34:15 +0100

    main: add first canvas with a green rectangle

--- vuetify-canvas/src/components/HelloWorld.vue
+++ vuetify-canvas/src/components/HelloWorld.vue
@@ -1,7 +1,25 @@
 <template>
-    <span>hello</span>
+  <canvas id="tutorial" width="800" height="600"></canvas>
 </template>
 
 <script lang="ts" setup>
-//
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const canvas = document.getElementById("tutorial");
+  if (canvas instanceof HTMLCanvasElement) {
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+      ctx.fillStyle = "green";
+      ctx.fillRect(20, 10, 150, 100);
+    }
+
+  }
+
+})
 </script>
+<style scoped>
+canvas {
+  border: 1px solid black;
+}
+</style>
